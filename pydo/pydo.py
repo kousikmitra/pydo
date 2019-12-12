680a8cc6a81084d53d95848e08d039d0cdd1fdbf
kousik <kousikmitra12@gmail.com>
2019-12-12 14:26:32 +0530

    Now shows inspiration quote on pydo --inspire

--- pydo/pydo.py
+++ pydo/pydo.py
@@ -1,5 +1,8 @@
 import argparse
 import sys
+from os.path import dirname, abspath, join
+from random import randrange
+import json
 
 class MyParser(argparse.ArgumentParser):
     def error(self, message):
@@ -7,11 +10,20 @@ class MyParser(argparse.ArgumentParser):
         self.print_help()
         sys.exit(2)
 
+
+def inspire():
+    inspire_file = join(dirname(dirname(abspath(__file__))), 'static', 'inspiration.json')
+    with open(inspire_file, 'r') as f:
+        data = json.load(f)
+        print(data[randrange(len(data))])
+
 def main():
     parser = MyParser(prog ='pydo', description ='A python command line todo app') 
   
     parser.add_argument('--say', dest='say', type=str,
-                        help='Echos the sentence') 
+                        help='Echos the sentence')
+    
+    parser.add_argument('--inspire', dest='inspire', action='store_true', help='Will tell you an inspiration quote')
 
     if len(sys.argv)==1:
         parser.print_help(sys.stderr)
@@ -21,7 +33,13 @@ def main():
   
     if args.say: 
         print("Echo: ", args.say)
+    elif args.inspire:
+        inspire()
     elif args.save:
         print("Saving task...")
     else:
         print("What? :|")
+
+
+if __name__ == "__main__":
+    main()
